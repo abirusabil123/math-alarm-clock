2a609a8aa1e82ee07d5cb9336dcb03c501aa8d3f
Mohammad Sheraj <mdsheraj123@gmail.com>
2026-04-18 19:50:48 +0530

    Release 2.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "2.4.1"
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
